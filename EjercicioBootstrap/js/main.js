a2ba49671ae4c7adb98dd9574c2704e2587715f6
AgustinGabWeRum <agustinweiske@gmail.com>
2018-10-17 19:16:24 +0200

    Terminar display none en grave o leve

--- EjercicioBootstrap/js/main.js
+++ EjercicioBootstrap/js/main.js
@@ -3,4 +3,20 @@ function onSubmit( form ){
     var data = JSON.stringify( $(form).serializeArray() ); //  <-----------
     console.log( data );
     return false; //don't submit
-  }
+}
+window.onload = function(){
+  var fecha = new Date(); //Fecha actual
+  var mes = fecha.getMonth()+1; //obteniendo mes
+  var dia = fecha.getDate(); //obteniendo dia
+  var ano = fecha.getFullYear(); //obteniendo año
+  if(dia<10)
+    dia='0'+dia; //agrega cero si el menor de 10
+  if(mes<10)
+    mes='0'+mes //agrega cero si el menor de 10
+  document.getElementById('incident').value=ano+"-"+mes+"-"+dia;
+  document.getElementById('dias').value=ano+"-"+mes+"-"+dia;
+  document.getElementById('horas').value=ano+"-"+mes+"-"+dia;
+  document.getElementById('dias2').value=ano+"-"+mes+"-"+dia;
+  document.getElementById('dias3').value=ano+"-"+mes+"-"+dia;
+  document.getElementById('dias4').value=ano+"-"+mes+"-"+dia;
+}
