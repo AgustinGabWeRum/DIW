488f912416fd558bf10ec9d3f2f3e6d4f771f23e
AgustinGabWeRum <agustinweiske@gmail.com>
2018-11-19 17:25:10 +0100

    He hecho el metodo de borrar pero aun no me funciona

--- EjercicioBootstrap/public/detalles.js
+++ EjercicioBootstrap/public/detalles.js
@@ -1,10 +1,18 @@
 var AuxID=location.search.substr(1).split("=")[1];
+var leve;
+var grave;
 $.getJSON( "http://localhost:3000/partes/"+AuxID, function( data ) {
   var items = [];
     $.each( data, function( key, val ) {
   console.log(key);
   
     items.push( "<li id='" + key + "' class='list-group-item'>" +key+" : " + val + "</li>" );
+
+    if( key == "leve"){
+      leve = val;
+    }else{
+      grave = val;
+    }
   });
   
   $( "<ul/>", {
@@ -12,3 +20,22 @@ $.getJSON( "http://localhost:3000/partes/"+AuxID, function( data ) {
     html: items.join( "" )
   }).appendTo( "#divLista" );
 });
+
+function eliminar(){
+  alert("ye");
+    $.ajax({
+      type: "DELETE",
+      url: "http://localhost:3000/partes/"+AuxID,
+      data: ""
+    });
+}
+
+function cambiarPag(){
+
+  if(grave == "grave"){
+    document.getElementById('enlace').href = "http://localhost:3000/listarGrave.html";
+  }else{
+    document.getElementById('enlace').href = "http://localhost:3000/listarLeve.html";
+  }
+}
+  
